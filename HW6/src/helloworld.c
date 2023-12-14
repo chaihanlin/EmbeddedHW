8dee58405d37dea0adb2753fe9347672944f65ef
chaihanlin <987821630@qq.com>
2023-12-14 11:06:04 +0800

    final

--- HW6/src/helloworld.c
+++ HW6/src/helloworld.c
@@ -1,19 +1,4 @@
 
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
-
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
@@ -28,34 +13,41 @@
 #define CR      0x100
 #define TX_FIFO 0x108
 #define RX_FIFO 0x10C
+#define TX_FIFO_OCY 0x114
+#define RX_FIFO_OCY 0x118
 
 void AXI_IIC_Config();
-
 int main()
 {
     init_platform();
 
-    unsigned int rxBuffer;
-
-    AXI_IIC_Config();
+     uint32_t  rxBuffer,rxBuffer1;
+     AXI_IIC_Config();
+     while(1){
 
 	Xil_Out32((IIC_Base_Addr + TX_FIFO), (0x100 | (i2cAddr<<1 )));
-	Xil_Out32((IIC_Base_Addr + TX_FIFO), 0x0B);
+	Xil_Out32((IIC_Base_Addr + TX_FIFO), 0x00);
 	Xil_Out32((IIC_Base_Addr + TX_FIFO), (0x101 | (i2cAddr<<1 )));
-	Xil_Out32((IIC_Base_Addr + TX_FIFO), 0x200 + 2);
+	Xil_Out32((IIC_Base_Addr + TX_FIFO), 0x200 + rxSize);
+ 	Xil_Out32(IIC_Base_Addr+CR,Xil_In32(IIC_Base_Addr+CR)|0x0d);
+	rxBuffer = Xil_In32(IIC_Base_Addr + RX_FIFO);
+	for(int i=0;i<9;i++)
+			for(int j=0;j<99;j++);
+	rxBuffer1 = Xil_In32(IIC_Base_Addr + RX_FIFO) ;
 
-	rxBuffer = Xil_In32(IIC_Base_Addr + RX_FIFO) ;
+// 	xil_printf("%x\r\n",Xil_In32(IIC_Base_Addr + 0x104));
 
-	xil_printf("%d",rxBuffer);
+//	xil_printf("%x  %x\r\n",rxBuffer,rxBuffer1);
+	xil_printf("%d°C\r\n",((rxBuffer<<5)|(rxBuffer1>>3))>>4);
 
-	for(int i=0;i<999;i++)
+	for(int i=0;i<9990;i++)
 		for(int j=0;j<999;j++);
 
-
+   }
     cleanup_platform();
     return 0;
 }
 
 void AXI_IIC_Config(){
-    Xil_Out32(IIC_Base_Addr+CR,Xil_In32(IIC_Base_Addr+CR)|0x01);
+ Xil_Out32(IIC_Base_Addr+CR,Xil_In32(IIC_Base_Addr+CR)|0x01);
 }
